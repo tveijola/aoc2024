dafa0bf13a99e600e2706208609bf05cbab47216
tveijola <tommi.veijola@gmail.com>
2024-12-07 11:22:24 +0200

    day 7

---
+++ day7/d7p2.py
@@ -0,0 +1,38 @@
+from itertools import product
+
+
+day = "day7"
+
+# filename = f"./{day}/sample"
+filename = f"./{day}/data"
+
+with open(filename) as f:
+    puzzle_input = [line.strip() for line in f.readlines()]
+
+PLUS = "+"
+MULT = "*"
+CONC = "|"
+OPERANDS = [PLUS, MULT, CONC]
+
+def evaluate_perm(nums: list[int], perm: tuple[str, ...]):
+    val, rest = nums[0], nums[1:]
+    for num, oper in zip(rest, perm):
+        if (oper == PLUS):
+            val += num
+        elif (oper == MULT):
+            val *= num
+        elif (oper == CONC):
+            val = int(str(val) + str(num))
+    return val
+
+def evaluate(line: str):
+    tokens = line.split(":")
+    test_val = int(tokens[0])
+    nums = [int(x) for x in tokens[1].split()]
+    num_oper = len(nums) - 1
+    for perm in product(OPERANDS, repeat=num_oper):
+        if test_val == evaluate_perm(nums, perm):
+            return test_val
+    return 0
+
+print(sum(evaluate(line) for line in puzzle_input))
