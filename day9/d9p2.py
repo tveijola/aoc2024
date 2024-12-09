f5cb88ead7887d4ed02a8fa37e0c4faa404fe4ba
tveijola <tommi.veijola@gmail.com>
2024-12-09 18:11:29 +0200

    use lists instead of dict

--- day9/d9p2.py
+++ day9/d9p2.py
@@ -7,8 +7,8 @@ with open(filename) as file:
     puzzle_input = file.read().strip()
 
 # mem_location, block_len, id_number
-files = dict()
-free = dict()
+files = []
+free = []
 
 is_file = True
 pointer = 0
@@ -16,31 +16,32 @@ idx = 0
 for c in puzzle_input:
     block_len = int(c)
     if is_file:
-        files[idx] = (pointer, block_len, idx)
-    else:
-        free[idx] = (pointer, block_len, None)
+        files.append((pointer, block_len, idx))
         idx += 1
+    else:
+        free.append((pointer, block_len, None))
     pointer += block_len
     is_file = not is_file
 
 print(f"start, len filesys={len(files)}")
 for i in range(len(files) - 1, 0, -1):
     file = files[i]
-    for key, val in free.items():
-        if val[0] > file[0]:
+    for j in range(len(free)):
+        free_block = free[j]
+        if free_block[0] > file[0]:
             break
-        if val[1] >= file[1]: # file fits
-            new_file = (val[0], file[1], file[2])
-            new_free = (val[0] + file[1], val[1] - file[1], val[2])
+        if free_block[1] >= file[1]: # file fits
+            new_file = (free_block[0], file[1], file[2])
+            new_free = (free_block[0] + file[1], free_block[1] - file[1], free_block[2])
             files[i] = new_file
-            free[key] = new_free
+            free[j] = new_free
             break
 
 total = 0
-for val in files.values():
-    start = val[0]
-    block_len = val[1]
-    id_num = val[2]
+for file in files:
+    start = file[0]
+    block_len = file[1]
+    id_num = file[2]
     for i in range(block_len):
         total += (start + i) * id_num
 print(total)
